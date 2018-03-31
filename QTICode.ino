f4a1f476280d1b8c30a9965ec26632a484ca9033
smarshack <37962272+smarshack@users.noreply.github.com>
2018-03-31 19:30:03 -0400

    Add files via upload

---
+++ QTICode.ino
@@ -0,0 +1,28 @@
+
+int ls1; 
+
+void setup() {
+ Serial.begin(9600);
+
+}
+
+void loop() {
+
+  ls1 = RCTime(6); // replace this with the pin you attatched the WHITE wire of the LEFT qti sensor too
+ Serial.println(ls1);
+
+ 
+}
+
+long RCTime(int sensorIn){
+   long duration = 0;
+   pinMode(sensorIn, OUTPUT);     
+   digitalWrite(sensorIn, HIGH);  
+   delay(1);                      
+   pinMode(sensorIn, INPUT);      
+   digitalWrite(sensorIn, LOW);   
+   while(digitalRead(sensorIn)){  
+      duration++;
+   }
+   return duration;
+}
